ceaf0f367ca2f5d66e3f83a39b44fb7251ba0741
Ahmed mazen dessouky <amd07dev@gmail.com>
2025-06-18 11:48:19 +0200

    test

--- client/app/[locale]/admin/page.tsx
+++ client/app/[locale]/admin/page.tsx
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function AdminPage() {
-  return <main>Admin Page again test again and again and last one again </main>
+  return <main>Admin Page</main>
 }
